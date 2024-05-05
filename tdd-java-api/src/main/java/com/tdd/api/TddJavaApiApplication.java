2b026123ce0072ec5e8599db21fbc23254842eca
AnderGI <agibarguen@gmail.com>
2024-05-05 12:32:03 +0200

    Initial setup

---
+++ tdd-java-api/src/main/java/com/tdd/api/TddJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddJavaApiApplication.class, args);
+	}
+
+}
