f3a33ab6089dfa3a7c4cb726fb84b44bd5d8b4b0
AnderGI <agibarguen@gmail.com>
2024-05-16 09:16:43 +0200

    InMemoryRepositoryIntegrationTesting refactored, object mother applied, and semantic functions added following given when then

--- tdd-java-api/src/test/java/infrastructure/database/inmemory/InMemoryRepositoyIntegrationTesting.java
+++ tdd-java-api/src/test/java/infrastructure/database/inmemory/InMemoryRepositoyIntegrationTesting.java
@@ -2,15 +2,22 @@ package infrastructure.database.inmemory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.Mockito;
 
-import application.get.CourseGetter;
-import application.get.CourseSaver;
+import application.find_course.CourseFinder;
+import application.save_course.CourseSaver;
 import domain.Course;
 import domain.CourseId;
+import domain.CourseIdMother;
+import domain.CourseMother;
+import domain.CourseNotExistError;
+import domain.CourseRepository;
 import domain.CourseTitle;
 import domain.InvalidArgumentException;
 import infrastructure.InMemoryCourseRepository;
@@ -18,46 +25,50 @@ import infrastructure.InMemoryCourseRepository;
 @TestMethodOrder(MethodOrderer.Random.class)
 public class InMemoryRepositoyIntegrationTesting {
 	@Test
-	void it_should_add_a_course() {
-		InMemoryCourseRepository repo = new InMemoryCourseRepository();
-		CourseSaver useCase = new CourseSaver(repo);
-		Course toRetrieveCourse = null;
-		try {
-			toRetrieveCourse = Course.createFromPrimitives("a87df656-c710-416d-81b6-fe341c2589e8","Course Title");
-		} catch (InvalidArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		useCase.saveCourse(toRetrieveCourse);
-	}
-	@Test
-	void it_should_get_an_existing_course_from_database() {
-		InMemoryCourseRepository repo = new InMemoryCourseRepository();
-		// Save a course
-		Course newCourse = null;
-		try {
-			newCourse = Course.createFromPrimitives("a87df656-c710-416d-81b6-fe341c2589e8","Course Title");
-		} catch (InvalidArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		CourseSaver saver = new CourseSaver(repo);
-		saver.saveCourse(newCourse);
-		// Get the course
-		CourseGetter getter = new CourseGetter(repo);
-		Course retrievedCourse = getter.getCourseById(newCourse.getIdValue());
-		assertEquals(newCourse, retrievedCourse);
+	// wont throw any exception
+	void it_should_add_a_course() throws InvalidArgumentException {
+		CourseRepository courseRepo = this.givenAnInMemoryCourseRepository();
+		CourseSaver courseSaver = this.givenACourseSaver(courseRepo);
+		Course newValidCourse = CourseMother.create();
+		courseSaver.saveCourse(newValidCourse);
 	}
 
 	@Test
-	void it_should_not_get_an_inexisting_course_from_database() {
-		InMemoryCourseRepository repo = new InMemoryCourseRepository();
-		CourseGetter useCase = new CourseGetter(repo);
-		Course retrievedCourse = useCase.getCourseById("fakeId");
-		assertEquals(null, retrievedCourse);
+	void it_should_get_an_existing_course_from_database() throws InvalidArgumentException, CourseNotExistError {
+		// Given
+		CourseRepository courseRepo = this.givenAnInMemoryCourseRepository();
+		CourseSaver courseSaver = this.givenACourseSaver(courseRepo);
+		Course newValidCourse = CourseMother.create();
+		courseSaver.saveCourse(newValidCourse);
+		CourseFinder courseFinder = this.givenACourseFinder(courseRepo);
+		String courseIdValue = newValidCourse.getIdValue();
+		// When method get is called
+		Course retreivedCourse = courseFinder.findCourse(CourseIdMother.fromValue(courseIdValue));
+		// Then
+		assertSame(newValidCourse, retreivedCourse);
 	}
-	
 
-	
+	@Test
+	void it_should_not_get_an_inexisting_course_from_database() throws InvalidArgumentException {
+		// Given
+		CourseRepository courseRepo = this.givenAnInMemoryCourseRepository();
+		Course newValidCourse = CourseMother.create();
+		CourseFinder courseFinder = this.givenACourseFinder(courseRepo);
+		String courseIdValue = newValidCourse.getIdValue();
+		// When findCourse is called it will throw an error for inexistent courses
+		assertThrows(CourseNotExistError.class, 
+				() -> courseFinder.findCourse(CourseIdMother.fromValue(courseIdValue)));
+	}
+
+	private CourseRepository givenAnInMemoryCourseRepository() {
+		return new InMemoryCourseRepository();
+	}
+
+	private CourseSaver givenACourseSaver(CourseRepository repo) {
+		return new CourseSaver(repo);
+	}
+
+	private CourseFinder givenACourseFinder(CourseRepository repo) {
+		return new CourseFinder(repo);
+	}
 }
